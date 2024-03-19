b36d8f33d5a8d9873a66fa7a3e5643855e757c6c
GuilhermeRS <guilhermemuralha2016@gmail.com>
2024-03-19 13:42:37 -0300

    Mundando icones

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Botao from '../Botao';
 import CampoText from '../CampoText';
 import ListaSuspensa from '../ListaSuspensa';
--- src/components/Membro/index.js
+++ src/components/Membro/index.js
@@ -3,16 +3,19 @@ import { GiFullMotorcycleHelmet } from "react-icons/gi"; ///Piloto
 import { GrUserManager } from "react-icons/gr";  ///Chefe
 import "./Membro.css";
 //USANDO PROPS COM OBJETO
-const Membro = ({nome, foto, posicao, corDeFundo, aoDeletar}) => {
+const Membro = ({ nome, foto, posicao, corDeFundo, aoDeletar }) => {
   return (
     <div className="membro">
-      <AiTwotoneCloseCircle size={20} className="excluir-membro" onClick={aoDeletar}/>
-      <div className="cabecalho" style={{backgroundColor:corDeFundo}}>
+      <AiTwotoneCloseCircle size={20} className="excluir-membro" onClick={aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={foto} alt={nome} />
       </div>
       <div className="rodape">
         <h4>{nome}</h4>
-        <h5><GiFullMotorcycleHelmet size={20}/>{posicao}</h5>
+        {posicao != "Chefe de Equipe" ? <h5><GiFullMotorcycleHelmet size={20} />{posicao}</h5> :
+          <h5><GrUserManager size={20} />{posicao}</h5>
+        }
+
       </div>
     </div>
   );
